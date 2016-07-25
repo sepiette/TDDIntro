4a9fc329e06b94f7f147a82d79da7c943b291d40
Sara Piette <sepiette@gmail.com>
2016-07-25 12:11:01 -0400

    refactor test code so only one Account instance

--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -1,26 +1,30 @@
 package com.thoughtworks.tddintro.accountbalance;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AccountTests {
 
+    private Account account;
+    @Before
+    public void setUp(){
+        account = new Account(100);
+    }
+
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney() {
-        Account account = new Account(100);
         assertThat(account.depositMoney(50), is(150));
-
     }
 
     @Test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-        Account account = new Account(100);
         assertThat(account.withdrawMoney(50), is(50));
     }
 //
 //    @Test
 //    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
 //
-//    }
+//
 }
