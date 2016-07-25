0e80744a6210836fd0579fc1e46842320e5cf163
Sara Piette <sepiette@gmail.com>
2016-07-25 11:47:50 -0400

    factorial TDD exercise coded

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -1,7 +1,16 @@
 package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        return -1;
+    public Integer compute(int i) throws IllegalArgumentException{
+        if(i == 0)
+            return 1;
+        if(i < 0){
+            throw new IllegalArgumentException("Must be positive number.");
+        }
+        int result = i;
+        for(int count = i-1; count>0;count--){
+            result *= count;
+        }
+        return result;
     }
 }
